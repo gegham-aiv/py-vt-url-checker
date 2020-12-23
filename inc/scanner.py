66de442c53058edfd079e69a87650c40a5016377
Gegham Ayvazyan <gegham.aivazyan@fifth-llc.com>
2020-12-24 00:02:57 +0400

    Code improvements

--- inc/scanner.py
+++ inc/scanner.py
@@ -6,11 +6,14 @@ from inc.helpers import output_info as info, get_out_file_name
 
 
 class Scanner:
-    def __init__(self, path, num):
+    def __init__(self, path, num, verbose = False):
         self.path = path
         self.client = VTClient()
         self.num = num
         self.out_path = 'out'
+        self.entities_scanned = 0
+        self.needed_entities_scanned = False
+        self.print = verbose
         # create the directory for saving output if does not exist
         if not os.path.isdir(self.out_path):
             os.makedirs(self.out_path)
@@ -21,42 +24,20 @@ class Scanner:
         # filtering the files which have been marked as "done"
         files = list(filter(lambda f: not f.endswith('.done'), files))
 
-        entities_scanned = 0
-        provided_entities_number_scanned = False
-
-        info(f"Total {len(files)} files detected")
-        vt_client = VTClient()
+        self.print_info(f"Total {len(files)} files detected")
         for file in files:
-            if provided_entities_number_scanned:
+            if self.needed_entities_scanned:
                 break
-            info(f"Reading file {file}")
+            self.print_info(f"Reading file {file}")
             file_path = f"{self.path}/{file}"
             file_handler = open(file_path, 'r')
             hostnames = file_handler.readlines()
 
             for hostname in hostnames:
-                if entities_scanned >= self.num:
-                    provided_entities_number_scanned = True
+                if self.entities_scanned >= self.num:
+                    self.needed_entities_scanned = True
                     break
-                hostname = hostname.strip()
-                out_file_name = get_out_file_name(hostname, file)
-                if os.path.isfile(out_file_name):
-                    # this hostname has already been scanned
-                    continue
-
-                info(f"Checking hostname: {hostname}")
-                results = vt_client.check_host(hostname)
-                negatives = results['malicious'] + results['suspicious']
-                message = 'Not Suspicious'
-                if negatives > 0:
-                    message = f'{negatives} services reported the host as malicious / suspicious'
-                info(message)
-                data_to_save = {hostname: message}
-                json_data = json.dumps(data_to_save, sort_keys=True, indent=4)
-                json_file_handler = open(out_file_name, "w+")
-                json_file_handler.write(json_data)
-                json_file_handler.close()
-                entities_scanned += 1
+                self.scan_hostname(hostname, file)
 
             file_handler.close()
             os.rename(file_path, f'{file_path}.done')
@@ -66,3 +47,35 @@ class Scanner:
             raise ValueError('Provided path is not a valid directory')
 
         return [f for f in os.listdir(self.path) if os.path.isfile(os.path.join(self.path, f))]
+
+    def scan_hostname(self, hostname, file):
+        hostname = hostname.strip()
+        out_file_name = get_out_file_name(hostname, file)
+        if os.path.isfile(out_file_name):
+            # this hostname has already been scanned
+            return
+
+        self.print_info(f"Checking hostname: {hostname}")
+        results = self.client.check_host(hostname)
+        self.write_data_to_file(hostname, out_file_name, results)
+
+
+    def write_data_to_file(self, hostname, file_name,  data):
+        negatives = data['malicious'] + data['suspicious']
+        message = 'Not Suspicious'
+        if negatives > 0:
+            message = f'{negatives} services reported the host as malicious / suspicious'
+        self.print_info(message)
+        data_to_save = {hostname: message}
+        json_data = json.dumps(data_to_save, sort_keys=True, indent=4)
+        json_file_handler = open(file_name, "w+")
+        json_file_handler.write(json_data)
+        json_file_handler.close()
+        self.entities_scanned += 1
+
+
+    def print_info(self, message):
+        if not self.print:
+            return
+        info(message)
+
