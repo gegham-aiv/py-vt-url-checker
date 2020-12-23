b3347c4d826f2502da180e113c5fd45c8c6210ba
Gegham Ayvazyan <gegham.aivazyan@fifth-llc.com>
2020-12-24 00:14:07 +0400

    Improvements on scanner

--- inc/scanner.py
+++ inc/scanner.py
@@ -6,7 +6,7 @@ from inc.helpers import output_info as info, get_out_file_name
 
 
 class Scanner:
-    def __init__(self, path, num, verbose = False):
+    def __init__(self, path, num, verbose=False):
         self.path = path
         self.client = VTClient()
         self.num = num
@@ -31,16 +31,24 @@ class Scanner:
             self.print_info(f"Reading file {file}")
             file_path = f"{self.path}/{file}"
             file_handler = open(file_path, 'r')
-            hostnames = file_handler.readlines()
+            hostnames = list(filter(lambda n: not self.hostname_is_scanned(n, file), file_handler.readlines()))
 
+            hostnames_scanned = 0
             for hostname in hostnames:
                 if self.entities_scanned >= self.num:
                     self.needed_entities_scanned = True
                     break
                 self.scan_hostname(hostname, file)
+                hostnames_scanned += 1
 
             file_handler.close()
-            os.rename(file_path, f'{file_path}.done')
+            if hostnames_scanned == len(hostnames):
+                os.rename(file_path, f'{file_path}.done')
+
+    def hostname_is_scanned(self, hostname, path):
+        # if file exists, then it's already scanned
+        filename = get_out_file_name(hostname.strip(), path)
+        return os.path.isfile(filename)
 
     def get_files(self):
         if not os.path.isdir(self.path):
@@ -50,17 +58,13 @@ class Scanner:
 
     def scan_hostname(self, hostname, file):
         hostname = hostname.strip()
-        out_file_name = get_out_file_name(hostname, file)
-        if os.path.isfile(out_file_name):
-            # this hostname has already been scanned
-            return
-
         self.print_info(f"Checking hostname: {hostname}")
         results = self.client.check_host(hostname)
-        self.write_data_to_file(hostname, out_file_name, results)
+        self.write_data_to_file(hostname, file, results)
 
+    def write_data_to_file(self, hostname, file_name, data):
+        out_file_name = get_out_file_name(hostname, file_name)
 
-    def write_data_to_file(self, hostname, file_name,  data):
         negatives = data['malicious'] + data['suspicious']
         message = 'Not Suspicious'
         if negatives > 0:
@@ -68,14 +72,12 @@ class Scanner:
         self.print_info(message)
         data_to_save = {hostname: message}
         json_data = json.dumps(data_to_save, sort_keys=True, indent=4)
-        json_file_handler = open(file_name, "w+")
+        json_file_handler = open(out_file_name, "w+")
         json_file_handler.write(json_data)
         json_file_handler.close()
         self.entities_scanned += 1
-
 
     def print_info(self, message):
         if not self.print:
             return
         info(message)
-
